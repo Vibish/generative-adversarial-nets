2ca2d743c29d7e77f100515ba9d44632a5a0e6f2
gdouzas <gdouzas@icloud.com>
2017-05-20 04:05:04 +0300

    Added setup script

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='ganetwork',
+    version='0.1.0',
+    py_modules='ganetwork',
+    author='Georgios Douzas',
+    author_email='gdouzas@icloud.com',
+)
