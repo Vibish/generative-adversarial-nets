c697cc097e6395e8a06b7d2b6585e8516b066ae1
gdouzas <gdouzas@icloud.com>
2017-05-20 11:10:11 +0300

    Fixed bug on setup script

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='ganetwork',
     version='0.1.0',
-    py_modules='ganetwork',
+    py_modules=['ganetwork'],
     author='Georgios Douzas',
     author_email='gdouzas@icloud.com',
 )
